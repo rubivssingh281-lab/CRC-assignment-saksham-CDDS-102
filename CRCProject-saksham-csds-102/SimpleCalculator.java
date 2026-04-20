67e15036d61c7cb68e867e7d3253964a524c5faf
ClulessCodes <rubivssingh281@gmail.com>
2026-04-20 10:41:05 +0530

    Update SimpleCalculator.java

--- CRCProject-saksham-csds-102/SimpleCalculator.java
+++ CRCProject-saksham-csds-102/SimpleCalculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class SimpleCalculator{
+class SimpleCalci{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Simple Calculator");
